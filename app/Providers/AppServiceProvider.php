ff8425bcc7e359eaddca9bb220c5abdd986c2170
SalvaOph <salva__68@hotmail.com>
2025-01-18 22:28:04 -0600

    modifying app service provider to force https requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use App\Services\PdfExportService;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
